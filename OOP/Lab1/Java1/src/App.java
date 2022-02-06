76f9fbfb9a089afd4f5f1d322bbeb97e4815b2c8
YuriiChubenko <51861976+YuriiChubenko@users.noreply.github.com>
2022-02-06 19:43:06 +0200

    Replaced booleans with exceptions for errors
    Added exceptions to address issues  #1, #2, #3, #4 brought up by @ASDjonok. Minor fix of #5

--- OOP/Lab1/Java1/src/App.java
+++ OOP/Lab1/Java1/src/App.java
@@ -1,45 +1,49 @@
 public class App {
     public static void main(String[] args) {
+
+        // Task constants
         final int C2 = 1;
         final int C3 = 1;
         final int C5 = 1;
         final int C7 = 5;
         final int C = C3;
+
         int a = 0;
         int b = 0;
         int m = 0;
         int n = 0;
-        boolean error = false;
+
         try {
             a = Integer.parseInt(args[0]);
             b = Integer.parseInt(args[1]);
             n = Integer.parseInt(args[2]);
             m = Integer.parseInt(args[3]);
+
         } catch (Exception e) {
-            System.out.println("Method expected 4 integers as argument");
-            error = true;
+            throw new IllegalArgumentException("Method expected 4 integers as argument");
 
         }
 
-        float s = 0;
-        if (!error) {
+        if (a > n || b > m) {
+            throw new IllegalArgumentException("Sum rule breached");
+        }
+
+        try {
+            float s = 0;
             for (float i = a; i <= n; i++) {
                 if (i + C == 0) {
-                    System.out.println("Error! Division by zero.");
-                    error = true;
-                    break;
+                    throw new ArithmeticException("Division by zero (i+C = 0).");
                 }
                 for (float j = b; j <= m; j++) {
+                    if (j == 0) {
+                        throw new ArithmeticException("Division by zero (j = 0).");
+                    }
                     s += (i / j) / (i - C);
                 }
             }
-            if (!error) {
-                System.out.println(s);
-            }
-        }
-
-        else {
-            System.out.println("Computation interrupted");
+            System.out.println("Computation done. Result: " + s);
+        } catch (Exception e) {
+            System.out.println("Computation interrupted. Reason: " + e.getMessage());
         }
 
     }
